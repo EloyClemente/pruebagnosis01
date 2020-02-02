0b345f9e9b011115a021a8e82659010630425508
Eloy <eloyclemente@gmail.com>
2020-02-02 19:14:02 +0100

    asd

--- js/main.js
+++ js/main.js
@@ -10,12 +10,14 @@ for(let fila of filas)
 		{
 			fila.style.height = this.children[1].offsetHeight + 25 + 'px'
 			fila.style.marginBottom = '20px'
+			fila.style.height = this.children[0].style.color = 'royalblue'
 		}
 		else
 		{
 			fila.style.height = '25px'
 			fila.style.marginBottom = '0'
 			fila.style.height = this.children[0].style.color = '#333'
+			console.log(fila.style.height = this.children[0])
 		}
 	})
 }
